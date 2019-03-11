3b6f390d27f42e6f09d3ed7040e4117fd97ac10b
really165 <31826004+really165@users.noreply.github.com>
2019-03-11 11:24:41 -0600

    single image test

--- test.py
+++ test.py
@@ -4,19 +4,7 @@ import numpy as np
 #import tkinter as tk
 #import maestro
 import time
-
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-
-# allow the camera to warmup
-time.sleep(0.1)
+import picamera
 
 MOTORS = 1
 TURN = 2
@@ -78,70 +66,77 @@ def turnRight():
     print('stop ' + str(turn))
     time.sleep(waitValue)
 
-#img = cv.imread("turnRight.jpg", cv.IMREAD_COLOR)
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    img = frame.array
-    #step one: edges
-    gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+#img = cv.imread("problemimage.jpg", cv.IMREAD_COLOR)
+from picamera.array import PiRGBArray
+import picamera
 
-    #step two: threshold
-    ret,threshold = cv.threshold(gray,190,255,cv.THRESH_BINARY)
-    img2, contours, hierarchy = cv.findContours(threshold,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('foo.jpg')
 
-    numberOfContours = 0
-    areaTolerance = 100
-    xTotal = 0
-    yTotal = 0
-    for contour in contours:
-        #if the contour is big enough
-        if(cv.contourArea(contour)>areaTolerance):
-            numberOfContours += 1
-            # compute the center of the contour
-            M = cv.moments(contour)
-            cX = int(M["m10"] / M["m00"])
-            xTotal += cX
-            cY = int(M["m01"] / M["m00"])
-            yTotal += cY
-        
-            # draw the contour and center of the shape on the image
-            cv.drawContours(img, [contour], -1, (0, 255, 0), 2)
-            cv.circle(img, (cX, cY), 7, (0, 0, 0), -1)
-            cv.putText(img, "center", (cX - 20, cY - 20),
-                cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-    #if a contour was found
-    if(numberOfContours > 0):
-        xAverage = int(xTotal/numberOfContours)
-        yAverage = int(yTotal/numberOfContours)
-        print("Average center is (" + str(xAverage) + ", " + str(yAverage) + ")")
+img = cv.imread("foo.jpg", cv.IMREAD_COLOR)
 
-        height, width, channels = img.shape
-        middleOfScreen = width/2
-        turnTolerance = 50
-        #determine if we need to turn
-        #if the average is in the middle somewhat
-        if((middleOfScreen-turnTolerance)<xAverage<(middleOfScreen+turnTolerance)):
-            #go straight
-            print("go straight")
+#step one: edges
+gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+#step two: threshold
+ret,threshold = cv.threshold(gray,190,255,cv.THRESH_BINARY)
+img2, contours, hierarchy = cv.findContours(threshold,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+
+numberOfContours = 0
+areaTolerance = 100
+xTotal = 0
+yTotal = 0
+for contour in contours:
+    #if the contour is big enough
+    if(cv.contourArea(contour)>areaTolerance):
+        numberOfContours += 1
+        # compute the center of the contour
+        M = cv.moments(contour)
+        cX = int(M["m10"] / M["m00"])
+        xTotal += cX
+        cY = int(M["m01"] / M["m00"])
+        yTotal += cY
+    
+        # draw the contour and center of the shape on the image
+        cv.drawContours(img, [contour], -1, (0, 255, 0), 2)
+        cv.circle(img, (cX, cY), 7, (0, 0, 0), -1)
+        cv.putText(img, "center", (cX - 20, cY - 20),
+            cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+#if a contour was found
+if(numberOfContours > 0):
+    xAverage = int(xTotal/numberOfContours)
+    yAverage = int(yTotal/numberOfContours)
+    print("Average center is (" + str(xAverage) + ", " + str(yAverage) + ")")
+
+    height, width, channels = img.shape
+    middleOfScreen = width/2
+    turnTolerance = 50
+    #determine if we need to turn
+    #if the average is in the middle somewhat
+    if((middleOfScreen-turnTolerance)<xAverage<(middleOfScreen+turnTolerance)):
+        #go straight
+        print("go straight")
+        #forward()
+    else:
+        #find out if we need to turn left or right
+        if(xAverage < middleOfScreen):
+            print("turn left")
+            #turnLeft()
+            #forward()
+        elif(xAverage > middleOfScreen):
+            print("turn right")
+            #turnRight()
             #forward()
         else:
-            #find out if we need to turn left or right
-            if(xAverage < middleOfScreen):
-                print("turn left")
-                #turnLeft()
-                #forward()
-            elif(xAverage > middleOfScreen):
-                print("turn right")
-                #turnRight()
-                #forward()
-            else:
-                print("error")
-    else:
-        print("stop")
-        break
+            print("error")
+else:
+    print("stop")
 
-    cv.imshow("Foreground", img)
+cv.imshow("Foreground", img)
 
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+cv.waitKey(0)
+cv.destroyAllWindows()
